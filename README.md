1685e3c61eb5791cc1b0bf4823b4f2fd051e4f7b
skule1 <skule@sormo.net>
2021-11-18 15:02:54 +0100

    Oppdatering med 20kHz samlingsfrekvens

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Vifteregulator
 
-Denne skissen leer tempraturen fra en BMP280, koblet til en ESP8266 via I2C-bus
-Temperaturen T1 leses fra sensoren, mappes om fra 30-50 til 0-100 og pusbreddestyres
-en motorkrets med D1 Mini I2C Dual Motor Driver Shield (HG7881) og styrer en 12V vifte på et grafikkkort.
+Denne skissen leer tempraturen fra en BMP280, koblet til en ESP32 D1 mini via I2C-bus
+Temperaturen t1 leses fra sensoren, mappes om fra 30-70 til 0-150 og pusbreddestyres
+en Motor Shield  med D1 Mini I2C Dual Motor Driver Shield (HG7881)som styrer en 12V vifte på et grafikkkort.
 Viftemotor kobles til A0 og A1. 12 volt kobles + til VM og - til GND.
-Viften slår inn ved 30 grader og reguleres proporsjonalt til 50 grader.
+Viften slår inn ved 30 grader og reguleres proporsjonalt til 70 grader.
 Etter dette får viften full energi.
 Temperaturen leses av i seriemonitoren.
 2.mai 2021
+
+En spenningsregulator gir 5v til kontrollkortet fra 12 forsyning på motorshield-et.
+PWM-refekvensen settes til 20 0000 Hz , fra 1000Hz som var default. Før det støyet shield-det sjenerende.
--- arduino/vifte/vifte.ino
+++ arduino/vifte/vifte.ino
@@ -1,26 +1,26 @@
- /***************************************************************************
-Denne skissen leer tempraturen fra en BMP280, koblet til en ESP8266 via I2C-bus
-Temperaturen T1 leses fra sensoren, mappes om fra 30-50 til 0-100 og pusbreddestyres
-en motorkrets med D1 Mini I2C Dual Motor Driver Shield (HG7881) og styrer en 12V vifte på et grafikkkort.
-Viftemotor kobles til A0 og A1. 12 volt kobles + til VM og - til GND.
-Viften slår inn ved 30 grader og reguleres proporsjonalt til 50 grader.
-Etter dette får viften full energi.
-Temperaturen leses av i seriemonitoren.
-2.mai 2021
- ***************************************************************************/
+/***************************************************************************
+  Denne skissen leer tempraturen fra en BMP280, koblet til en ESP8266 via I2C-bus
+  Temperaturen T1 leses fra sensoren, mappes om fra 30-50 til 0-100 og pusbreddestyres
+  en motorkrets med D1 Mini I2C Dual Motor Driver Shield (HG7881) og styrer en 12V vifte på et grafikkkort.
+  Viftemotor kobles til A0 og A1. 12 volt kobles + til VM og - til GND.
+  Viften slår inn ved 30 grader og reguleres proporsjonalt til 50 grader.
+  Etter dette får viften full energi.
+  Temperaturen leses av i seriemonitoren.
+  2.mai 2021
+***************************************************************************/
 #include "WEMOS_Motor.h"
 
 int pwm;
 
 //Motor shiled I2C Address: 0x30
 //PWM frequency: 1000Hz(1kHz)
-Motor M1(0x30,_MOTOR_A, 1000);//Motor A
+Motor M1(0x30, _MOTOR_B, 20000); //Motor A
 
 
 
- 
+
 #define pinout  14  //GPIO14, D5
-int T1;
+int t1;
 int val;
 
 #include <Wire.h>
@@ -45,33 +45,37 @@ void setup() {
     while (1);
   }
 
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+  /* Default settings from datasheet. * /
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. * /
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling * /
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling * /
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. * /
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-                  pinMode(pinout,OUTPUT);
+
+//  pinMode(pinout, OUTPUT);
+  M1.setmotor(_CCW, 150);
 }
 
-void loop() {
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+//void loop() {}
 
- 
-    Serial.println();
-    T1= round(bmp.readTemperature());
-//T1=70;
-    if (T1>30)
-    {
-        val = map(T1, 30, 50, 0, 100);
-    //  analogWrite(pinout,val);
-       val=constrain(val,0,100);
-          M1.setmotor(_CCW, val);
-          Serial.println(val);
-    }
-    else
-      M1.setmotor(_STOP);
-    delay(2000);
+void loop() {
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+
+  Serial.println();
+  t1 = round(bmp.readTemperature());
+ //t1=70;
+  if (t1 > 30)
+  {
+    val = map(t1, 30, 70, 0, 150);
+//    analogWrite(pinout, val);
+   val = constrain(val, 0, 150);
+    M1.setmotor(_CCW, val);
+    Serial.println(val);
+  }
+  else
+    M1.setmotor(_STOP);
+  delay(2000);
 }
